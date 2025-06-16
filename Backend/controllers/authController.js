be2545bc83428a6188ab78d4926280b717b677d9
mramankhann <user.email:mramankhann@gmail.com>
2025-06-16 18:40:42 +0530

    setup bcrypt and salt

---
+++ Backend/controllers/authController.js
@@ -0,0 +1,37 @@
+const User = require("../models/user");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+exports.register = async (req, res) => {
+  const { username, email, password } = req.body;
+  try {
+    const existingUser = await User.findOne({ email });
+    if (existingUser) return res.status(400).json({ msg: "User already exists" });
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPass = await bcrypt.hash(password, salt);
+
+    const newUser = new User({ username, email, password: hashedPass });
+    await newUser.save();
+    res.status(201).json({ msg: "User registered successfully" });
+  } catch (err) {
+    res.status(500).json({ msg: "Server Error" });
+  }
+};
+
+
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) return res.status(404).json({ msg: "User not found" });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(401).json({ msg: "Invalid credentials" });
+
+    const token = jwt.sign({ id: user._id }, "secretjwt", { expiresIn: "1d" });
+    res.status(200).json({ token, user: { id: user._id, username: user.username, email: user.email } });
+  } catch (err) {
+    res.status(500).json({ msg: "Login failed" });
+  }
+};
--- Backend/index.js
+++ Backend/index.js
@@ -2,16 +2,21 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
+const authRoutes = require('./routes/authRoutes');
 const connectDB = require('./config/db');
 dotenv.config();
 connectDB();
 
+
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use('/api/auth', authRoutes);
+
+
 
 app.get('/', (req, res) => {
-    res.send('Welcome to the API');
+    res.send('Welcome to the BookSwap Application API');
 });
 
 const PORT = process.env.PORT || 5000;
