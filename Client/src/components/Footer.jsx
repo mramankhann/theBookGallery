c5dfbe43ff060d965427123045a217890b45aeb1
Roshni Jaiswal <roshnijaiswal82240@gmail.com>
2025-09-09 18:08:21 +0530

    Update Footer.jsx

--- Client/src/components/Footer.jsx
+++ Client/src/components/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer className="bg-blue-500 text-white py-4 text-center mt-10">
       <p className="text-sm md:text-base">
-        © 2025 <span className="font-semibold">theBookGallery</span>. Made with ❤️ by Aman
+        © 2025 <span className="font-semibold">theBookGallery</span>
       </p>
     </footer>
   );
